639caa541c4404a2a22cea3c9a4772c169a5b791
Niklas Heer <niklas.heer@gmail.com>
2017-02-10 09:48:51 +0100

    new dump

--- shop/resources/views/cars/create.blade.php
+++ shop/resources/views/cars/create.blade.php
@@ -3,15 +3,33 @@
     {{ csrf_field() }}
 
     <div class="form-group">
-        <label for="name" class="control-label">Name</label>
-        <input type="text" class="form-control" id="name">
+        <label for="titel" class="control-label">Titel</label>
+        <input type="text" class="form-control" id="titel">
     </div>
     <div class="form-group">
-        <label for="lyrics" class="control-label">Lyrics</label>
-        <textarea name="lyrics" class="form-control" id="lyrics"></textarea>
+        <label for="description" class="control-label">Beschreibung</label>
+        <textarea name="lyrics" class="form-control" id="description"></textarea>
     </div>
     <div class="form-group">
-        <label for="publish" class="control-label">Publish</label>
-        <input type="checkbox" name="publish" id="publish">
+        <label for="price" class="control-label">Preis</label>
+        <input type="text" class="form-control" id="price">
     </div>
+    <div class="form-group">
+        <label for="kw" class="control-label">KW</label>
+        <input type="text" class="form-control" id="kw">
+    </div>
+    <div class="form-group">
+        <label for="km" class="control-label">Kilometer</label>
+        <input type="text" class="form-control" id="km">
+    </div>
+    <div class="form-group">
+        <label for="color" class="control-label">Farbe</label>
+        <input type="text" class="form-control" id="color">
+    </div>
+    <div class="form-group">
+        <label for="model" class="control-label">Modell</label>
+        <input type="text" class="form-control" id="model">
+    </div>
+
+
 </form>
