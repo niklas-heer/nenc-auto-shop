4194a8e514057fd78a60231744b8540bbe1e631d
Niklas Heer <niklas.heer@gmail.com>
2017-02-24 10:16:33 +0100

    images added

---
+++ shop/database/migrations/2017_02_24_090428_create_image_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateImageTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cars_images', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('path');
+            $table->integer('car_id')->unsigned();
+            $table->foreign('car_id')->references('id')->on('cars');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
