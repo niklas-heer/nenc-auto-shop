de1d36783955d86edeacdd7d7d7b08fe07a00ff4
Eddcapone <magic-style@live.de>
2017-02-24 10:56:57 +0100

    Update create.blade.php

--- shop/resources/views/cars/create.blade.php
+++ shop/resources/views/cars/create.blade.php
@@ -30,8 +30,7 @@
         </div>
         
         <div class="form-group">
-            <label for="image" class="control-label">Bild</label>
-            {!! Form::file('image') !!}
+            Foto: {!! Form::file('image') !!}
         </div>
 
         <div class="form-group">
