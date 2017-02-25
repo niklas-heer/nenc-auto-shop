8c47c7ebb5a4f99bf6c63c3a2fab9ac6aa362c3c
Eduard Fekete <magic-style@live.de>
2017-02-25 12:52:31 +0100

    URLs gefixed

--- shop/resources/views/layouts/footer.blade.php
+++ shop/resources/views/layouts/footer.blade.php
@@ -5,11 +5,11 @@
           
             <h3>&copy; 2017 NENC</h3>
             
-            <a class="FooterButton" href="impressum">Impressum</a>
-            <a class="FooterButton" href="datenschutz">Datenschutz</a>
-            <a class="FooterButton" href="haftungsausschluss">Haftungsausschluss</a>
-            <a class="FooterButton" href="agb">AGB</a>
-            <a class="FooterButton" href="kontakt">Kontakt</a>
+            <a class="FooterButton" href="/nenc-auto-shop/shop/public/impressum">Impressum</a>
+            <a class="FooterButton" href="/nenc-auto-shop/shop/public/datenschutz">Datenschutz</a>
+            <a class="FooterButton" href="/nenc-auto-shop/shop/public/haftungsausschluss">Haftungsausschluss</a>
+            <a class="FooterButton" href="/nenc-auto-shop/shop/public/agb">AGB</a>
+            <a class="FooterButton" href="/nenc-auto-shop/shop/public/kontakt">Kontakt</a>
       </div>
     </nav>
 </footer>
