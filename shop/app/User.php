cb5b870159c7abd1c2d52434906edeff5dcb9184
Eduard Fekete <magic-style@live.de>
2017-03-01 20:57:47 +0100

    Password vergessen Funktion entfernt; Filter+Header responsive

--- shop/app/User.php
+++ shop/app/User.php
@@ -4,11 +4,15 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
-use Illuminate\Auth\Authenticatable;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
-class User extends Model implements AuthenticatableContract
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Model implements AuthenticatableContract, CanResetPasswordContract  
 {
-    use Authenticatable;
+    use Authenticatable, CanResetPassword, Notifiable;
     
     protected $fillable = ['name', 'email', 'password', 'created_at', 'updated_at'];
     protected $hidden = ['remember_token'];
